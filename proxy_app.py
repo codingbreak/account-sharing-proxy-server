d9e7a358654e8fee5d7fe9a5f414f5748b8dfd7d
votientu <votientu@gmail.com>
2021-09-26 23:51:30 +0200

    handle http methods and add test

--- proxy_app.py
+++ proxy_app.py
@@ -4,37 +4,41 @@ import requests
 
 
 app = Flask(__name__)  # Python web backend library
-SITE_NAME = 'https://stackoverflow.com/'
-# To remove traces of proxy server in the response from web server 
-excluded_headers = ['content-encoding',         
-                    'content-length',       # gzip
-                    'transfer-encoding',    # chunked
-                    'connection']           # keep-alive
+app.config["CACHE_TYPE"] = "null"
+
+excluded_headers = ['content-encoding',       
+                    'content-length',       
+                    'transfer-encoding',    
+                    'connection']   
+
+SITE_NAME = 'https://reqbin.com/'
+# SITE_NAME = 'https://stackoverflow.com/'       
 
 @app.route('/')
 def index():
-    return 'Account Sharing Server!'
+    return SITE_NAME
 
-@app.route('/<path:path>', methods=['GET', 'POST'])
+@app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
 def proxy(path):
-    # print(request.headers)
-    start = time.perf_counter()
+    # print("Request:", request)
+    # start = time.perf_counter()
 
-    if request.method == 'GET':
-        resp = requests.get(f'{SITE_NAME}{path}')
+    resp = requests.request(
+        method=request.method,
+        url=request.url.replace(request.host_url, SITE_NAME),
+        headers={key: value for (key, value) in request.headers if key != 'Host'},
+        data=request.get_data(),
+        cookies=request.cookies,
+        allow_redirects=False)
 
-    elif request.method == 'POST':
-        resp = requests.post(f'{SITE_NAME}{path}', json=request.get_json()) # file consent ?
-
+    # Remove hop-by-hop headers
     headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
-    # removed_headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() in excluded_headers]
-    # print(removed_headers)
+    
     response = Response(resp.content, resp.status_code, headers)
     
-    request_time = time.perf_counter() - start
-
-    print('Proxy - Target Server: {:.1f} ms'.format(resp.elapsed.total_seconds()*1000))
-    print('Others: {:.1f} ms'.format(request_time*1000-resp.elapsed.total_seconds()*1000))
+    # request_time = time.perf_counter() - start
+    # print('Proxy - Target Server: {:.1f} ms'.format(resp.elapsed.total_seconds()*1000))
+    # print('Others: {:.1f} ms'.format(request_time*1000-resp.elapsed.total_seconds()*1000))
     
     return response
 
