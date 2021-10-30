99ef880f7ea5b68e8d1e347e76f829b0fcb17a1f
doanguyen <doanguyen@users.noreply.github.com>
2021-10-30 08:56:50 +0200

    chore: format using black

--- proxy_app.py
+++ proxy_app.py
@@ -1,41 +1,49 @@
 import requests
 from flask import Flask, request, Response
 
-app = Flask(__name__, static_url_path='/nothing')  # Python web backend library
+app = Flask(__name__, static_url_path="/nothing")  # Python web backend library
 # app.config["CACHE_TYPE"] = "null"
 
 # hop-by-hop headers
-excluded_headers = ['content-encoding',
-                    'content-length',
-                    'transfer-encoding',
-                    'connection']
+excluded_headers = [
+    "content-encoding",
+    "content-length",
+    "transfer-encoding",
+    "connection",
+]
 
 # SITE_NAME = 'https://reqbin.com/'
-SITE_NAME = 'https://stackoverflow.com/'
+SITE_NAME = "https://stackoverflow.com/"
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    app.logger.info('query %s', SITE_NAME)
+    app.logger.info("query %s", SITE_NAME)
     return SITE_NAME
 
 
-@app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
+@app.route("/<path:path>", methods=["GET", "POST", "PUT", "PATCH", "DELETE"])
 def proxy(path):
-    app.logger.info('query %s%s with method %s', SITE_NAME, path, request.method)
+    app.logger.info("query %s%s with method %s", SITE_NAME, path, request.method)
 
     resp = requests.request(
         method=request.method,
         url=request.url.replace(request.host_url, SITE_NAME),
-        headers={key: value for (key, value) in request.headers if key != 'Host'},
+        headers={key: value for (key, value) in request.headers if key != "Host"},
         data=request.get_data(),
         cookies=request.cookies,
-        allow_redirects=False)
+        allow_redirects=False,
+    )
 
     # Remove hop-by-hop headers
-    headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
+    headers = [
+        (name, value)
+        for (name, value) in resp.raw.headers.items()
+        if name.lower() not in excluded_headers
+    ]
     response = Response(resp.content, resp.status_code, headers)
     return response
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
